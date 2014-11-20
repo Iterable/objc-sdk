294ba9912e1d6bf3d9ef93d5eca07335356b2a14
Ilya Brin <ilya@iterable.com>
2014-11-19 17:28:13 -0800

    stuff

--- Iterable-iOS-SDK/Iterable-iOS-SDK/IterableAPI.m
+++ Iterable-iOS-SDK/Iterable-iOS-SDK/IterableAPI.m
@@ -6,29 +6,43 @@
 //  Copyright (c) 2014 Iterable. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 
 #import "IterableAPI.h"
 
 
 @implementation IterableAPI {
-    NSString *endpoint;
     NSString *apiKey;
     NSString *email;
 }
 
+NSString * const endpoint = @"https://api.iterable.com/api/";
+
 - (id)initWithApiKey:(NSString *)key andEmail:(NSString *)eml
 {
     self = [super init];
-    self->endpoint = @"https://api.iterable.com/api/";
     self->apiKey = key;
     self->email = eml;
     return self;
 }
 
-- (NSString*)getUrlForAction:(NSString *)action
+- (NSString *)getUrlForAction:(NSString *)action
 {
     return [NSString stringWithFormat:@"%@%@?api_key=%@", endpoint, action, apiKey];
 }
+
+- (NSString *)dictToJson:(NSDictionary *)dict {
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dict
+                                                       options:0
+                                                         error:&error];
+    if (! jsonData) {
+        NSLog(@"dictToJson failed: %@", error);
+        return nil;
+    } else {
+        return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    }
+}
+
 
 @end
