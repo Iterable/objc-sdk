b43991b7bc023161255c884a93a29d29be6a53c6
Ilya Brin <ilya@iterable.com>
2014-11-19 16:29:35 -0800

    first commit

---
+++ Iterable-iOS-SDK/Iterable-iOS-SDK/Iterable_iOS_SDK.m
@@ -0,0 +1,13 @@
+//
+//  Iterable_iOS_SDK.m
+//  Iterable-iOS-SDK
+//
+//  Created by Ilya Brin on 11/19/14.
+//  Copyright (c) 2014 Iterable. All rights reserved.
+//
+
+#import "Iterable_iOS_SDK.h"
+
+@implementation Iterable_iOS_SDK
+
+@end
